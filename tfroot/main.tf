080c124e9cb933e135055a8b73c1786c2d9d3a1c
Tobias Hornberger <saibot1207@googlemail.com>
2024-09-17 12:49:58 +0200

    add basic main.tf

---
+++ tfroot/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "remote" {
+    hostname = "scalr-eval.scalr.io"
+    organization = "env-v0oh4o3tl6cs3uju0"
+
+    workspaces {
+      name = "test-kibab"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo 'Congrats on your first run!'"
+   }
+}
