65d599845de7454e65e063fbdcbc3f07b66ed07f
Ilya Bakulin <webmaster@kibab.com>
2024-09-17 15:00:30 +0200

    Disable deletion protection

--- tfroot/cloudrun_instance.tf
+++ tfroot/cloudrun_instance.tf
@@ -2,6 +2,7 @@ resource "google_cloud_run_v2_service" "pgadmin" {
   name     = "pgadmin"
   location = "us-central1"
   ingress = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
+  deletion_protection = false
 
 
  template {
